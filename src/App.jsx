9f4fe9d70328cc7919d8283a42c0fe6f44a006a4
akashmob <akash.tiwari@mobmaxime.com>
2023-06-10 22:53:03 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Navbar from './Components/Navbar'
+import UserDetails  from './Components/UserDetails'
+
+function App() {
+
+  return (
+    <>
+     <Navbar/>   
+     <UserDetails />
+    </>
+  )
+}
+
+export default App
